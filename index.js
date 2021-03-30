9405863732f55b605e2da618ecd09320d17a3d68
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-30 19:20:36 +0600

    48-6 Handle Inventory and how to store image on server

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.connect((err) => {
   app.post('/addProduct', (req, res) => {
     const products = req.body;
     // console.log(product);
-    productsCollection.insertMany(products).then((result) => {
+    productsCollection.insertOne(products).then((result) => {
       console.log(result.insertedCount);
       res.send(result.insertedCount);
     });
